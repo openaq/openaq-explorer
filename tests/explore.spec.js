9759f6394c750fa9b0b56f5207d7cffcbdc226b7
Gabriel Fosse <67290377+majesticio@users.noreply.github.com>
2022-12-20 23:37:09 -0600

    network call tests

--- tests/explore.spec.js
+++ tests/explore.spec.js
@@ -106,18 +106,270 @@ test.describe('explore page', () => {
     await expect(page.getByLabel('Show locations with Poor data coverage')).not.toBeChecked();
   });
 
-  test('change drop down and assert text', async ({ page }) => { 
+  test('Select PM10, assert network call and legend text', async ({ page }) => { 
+
     await expect(page.locator('body > div.map-legend > div > div.map-legend-section > div.map-legend-title > span.type-subtitle-3.text-smoke-120'))
     .toHaveText('PM 2.5 (µg/m³)'); // this looks for the text in the legend
-
+    
+    const requestPromise = page.waitForRequest('https://api.openaq.org/v2/locations/tiles/2/1/1.pbf?parameter=1');
     await page.getByRole('combobox').first().selectOption('1');
+    expect (await requestPromise).toBeTruthy();
     await expect(page.locator('body > div.map-legend > div > div.map-legend-section > div.map-legend-title > span.type-subtitle-3.text-smoke-120'))
     .toHaveText('PM10 (µg/m³)');
+  });
 
+  test('Select O₃, assert network call and legend text', async ({ page }) => { 
+
+    await expect(page.locator('body > div.map-legend > div > div.map-legend-section > div.map-legend-title > span.type-subtitle-3.text-smoke-120'))
+    .toHaveText('PM 2.5 (µg/m³)');
+    
+    const requestPromise = page.waitForRequest('https://api.openaq.org/v2/locations/tiles/2/3/2.pbf?parameter=3');
     await page.getByRole('combobox').first().selectOption('3');
+    expect (await requestPromise).toBeTruthy();
     await expect(page.locator('body > div.map-legend > div > div.map-legend-section > div.map-legend-title > span.type-subtitle-3.text-smoke-120'))
     .toHaveText('O₃ mass (µg/m³)');
   });
 
+  test('Select co mass, assert network call and legend text', async ({ page }) => { 
+
+    await expect(page.locator('body > div.map-legend > div > div.map-legend-section > div.map-legend-title > span.type-subtitle-3.text-smoke-120'))
+    .toHaveText('PM 2.5 (µg/m³)');
+    
+    const requestPromise = page.waitForRequest('https://api.openaq.org/v2/locations/tiles/2/1/1.pbf?parameter=4');
+    await page.getByRole('combobox').first().selectOption('4');
+    expect (await requestPromise).toBeTruthy();
+    await expect(page.locator('body > div.map-legend > div > div.map-legend-section > div.map-legend-title > span.type-subtitle-3.text-smoke-120'))
+    .toHaveText('CO mass (µg/m³)');
+  });
+
+  test('Select NO₂ mass, assert network call and legend text', async ({ page }) => { 
+
+    await expect(page.locator('body > div.map-legend > div > div.map-legend-section > div.map-legend-title > span.type-subtitle-3.text-smoke-120'))
+    .toHaveText('PM 2.5 (µg/m³)');
+    
+    const requestPromise = page.waitForRequest('https://api.openaq.org/v2/locations/tiles/2/0/2.pbf?parameter=5');
+    await page.getByRole('combobox').first().selectOption('5');
+    expect (await requestPromise).toBeTruthy();
+    await expect(page.locator('body > div.map-legend > div > div.map-legend-section > div.map-legend-title > span.type-subtitle-3.text-smoke-120'))
+    .toHaveText('NO₂ mass (µg/m³)');
+  });
+ 
+
+  test('Select SO₂ mass, assert network call and legend text', async ({ page }) => { 
+
+    await expect(page.locator('body > div.map-legend > div > div.map-legend-section > div.map-legend-title > span.type-subtitle-3.text-smoke-120'))
+    .toHaveText('PM 2.5 (µg/m³)');
+    
+    const requestPromise = page.waitForRequest('https://api.openaq.org/v2/locations/tiles/2/0/2.pbf?parameter=6');
+    await page.getByRole('combobox').first().selectOption('6');
+    expect (await requestPromise).toBeTruthy();
+    await expect(page.locator('body > div.map-legend > div > div.map-legend-section > div.map-legend-title > span.type-subtitle-3.text-smoke-120'))
+    .toHaveText('SO₂ mass (µg/m³)');
+  });
+
+  test('Select NO₂ ppm, assert network call and legend text', async ({ page }) => { 
+
+    await expect(page.locator('body > div.map-legend > div > div.map-legend-section > div.map-legend-title > span.type-subtitle-3.text-smoke-120'))
+    .toHaveText('PM 2.5 (µg/m³)');
+    
+    const requestPromise = page.waitForRequest('https://api.openaq.org/v2/locations/tiles/2/0/2.pbf?parameter=7');
+    await page.getByRole('combobox').first().selectOption('7');
+    expect (await requestPromise).toBeTruthy();
+    await expect(page.locator('body > div.map-legend > div > div.map-legend-section > div.map-legend-title > span.type-subtitle-3.text-smoke-120'))
+    .toHaveText('NO₂ (ppm)');
+  });
+
+  test('Select CO ppm, assert network call and legend text', async ({ page }) => {
+      
+    await expect(page.locator('body > div.map-legend > div > div.map-legend-section > div.map-legend-title > span.type-subtitle-3.text-smoke-120'))
+    .toHaveText('PM 2.5 (µg/m³)');
+    
+    const requestPromise = page.waitForRequest('https://api.openaq.org/v2/locations/tiles/2/0/2.pbf?parameter=8');
+    await page.getByRole('combobox').first().selectOption('8');
+    expect (await requestPromise).toBeTruthy();
+    await expect(page.locator('body > div.map-legend > div > div.map-legend-section > div.map-legend-title > span.type-subtitle-3.text-smoke-120'))
+    .toHaveText('CO (ppm)');
+  });
+
+  test('Select SO₂ ppm, assert network call and legend text', async ({ page }) => {
+        
+    await expect(page.locator('body > div.map-legend > div > div.map-legend-section > div.map-legend-title > span.type-subtitle-3.text-smoke-120'))
+    .toHaveText('PM 2.5 (µg/m³)');
+    
+    const requestPromise = page.waitForRequest('https://api.openaq.org/v2/locations/tiles/2/0/2.pbf?parameter=9');
+    await page.getByRole('combobox').first().selectOption('9');
+    expect (await requestPromise).toBeTruthy();
+    await expect(page.locator('body > div.map-legend > div > div.map-legend-section > div.map-legend-title > span.type-subtitle-3.text-smoke-120'))
+    .toHaveText('SO₂ (ppm)');
+  });
+
+  test('Select O₃ ppm, assert network call and legend text', async ({ page }) => {
+
+    await expect(page.locator('body > div.map-legend > div > div.map-legend-section > div.map-legend-title > span.type-subtitle-3.text-smoke-120'))
+    .toHaveText('PM 2.5 (µg/m³)');
+    
+    const requestPromise = page.waitForRequest('https://api.openaq.org/v2/locations/tiles/2/0/2.pbf?parameter=10');
+    await page.getByRole('combobox').first().selectOption('10');
+    expect (await requestPromise).toBeTruthy();
+    await expect(page.locator('body > div.map-legend > div > div.map-legend-section > div.map-legend-title > span.type-subtitle-3.text-smoke-120'))
+    .toHaveText('O₃ (ppm)');
+  });
+
+  test('Select BC (µg/m³), assert network call and legend text', async ({ page }) => {
+      
+    await expect(page.locator('body > div.map-legend > div > div.map-legend-section > div.map-legend-title > span.type-subtitle-3.text-smoke-120'))
+    .toHaveText('PM 2.5 (µg/m³)');
+    
+    const requestPromise = page.waitForRequest('https://api.openaq.org/v2/locations/tiles/2/0/2.pbf?parameter=11');
+    await page.getByRole('combobox').first().selectOption('11');
+    expect (await requestPromise).toBeTruthy();
+    await expect(page.locator('body > div.map-legend > div > div.map-legend-section > div.map-legend-title > span.type-subtitle-3.text-smoke-120'))
+    .toHaveText('BC (µg/m³)');
+  });
+
+  test('Select PM1 (µg/m³), assert network call and legend text', async ({ page }) => {
+          
+    await expect(page.locator('body > div.map-legend > div > div.map-legend-section > div.map-legend-title > span.type-subtitle-3.text-smoke-120'))
+    .toHaveText('PM 2.5 (µg/m³)');
+    
+    const requestPromise = page.waitForRequest('https://api.openaq.org/v2/locations/tiles/2/3/1.pbf?parameter=19');
+    await page.getByRole('combobox').first().selectOption('19');
+    expect (await requestPromise).toBeTruthy();
+    await expect(page.locator('body > div.map-legend > div > div.map-legend-section > div.map-legend-title > span.type-subtitle-3.text-smoke-120'))
+    .toHaveText('PM1 (µg/m³)');
+  });
+
+  test('Select CO₂ (ppm), assert network call and legend text', async ({ page }) => {
+                
+    await expect(page.locator('body > div.map-legend > div > div.map-legend-section > div.map-legend-title > span.type-subtitle-3.text-smoke-120'))
+    .toHaveText('PM 2.5 (µg/m³)');
+    
+    const requestPromise = page.waitForRequest('https://api.openaq.org/v2/locations/tiles/2/0/2.pbf?parameter=21');
+    await page.getByRole('combobox').first().selectOption('21');
+    expect (await requestPromise).toBeTruthy();
+    await expect(page.locator('body > div.map-legend > div > div.map-legend-section > div.map-legend-title > span.type-subtitle-3.text-smoke-120'))
+    .toHaveText('CO₂ (ppm)');
+  });
+
+  test('Select NOx mass (µg/m³), assert network call and legend text', async ({ page }) => {
+
+    await expect(page.locator('body > div.map-legend > div > div.map-legend-section > div.map-legend-title > span.type-subtitle-3.text-smoke-120'))
+    .toHaveText('PM 2.5 (µg/m³)');
+    
+    const requestPromise = page.waitForRequest('https://api.openaq.org/v2/locations/tiles/2/0/2.pbf?parameter=27');
+    await page.getByRole('combobox').first().selectOption('27');
+    expect (await requestPromise).toBeTruthy();
+    await expect(page.locator('body > div.map-legend > div > div.map-legend-section > div.map-legend-title > span.type-subtitle-3.text-smoke-120'))
+    .toHaveText('NOx mass (µg/m³)');
+  });
+
+  test('Select CH₄ (ppm), assert network call and legend text', async ({ page }) => {
+
+    await expect(page.locator('body > div.map-legend > div > div.map-legend-section > div.map-legend-title > span.type-subtitle-3.text-smoke-120'))
+    .toHaveText('PM 2.5 (µg/m³)');
+    
+    const requestPromise = page.waitForRequest('https://api.openaq.org/v2/locations/tiles/2/0/2.pbf?parameter=28');
+    await page.getByRole('combobox').first().selectOption('28');
+    expect (await requestPromise).toBeTruthy();
+    await expect(page.locator('body > div.map-legend > div > div.map-legend-section > div.map-legend-title > span.type-subtitle-3.text-smoke-120'))
+    .toHaveText('CH₄ (ppm)');
+  });
+
+  test('Select UFP count (particles/cm³), assert network call and legend text', async ({ page }) => {
+      
+    await expect(page.locator('body > div.map-legend > div > div.map-legend-section > div.map-legend-title > span.type-subtitle-3.text-smoke-120'))
+    .toHaveText('PM 2.5 (µg/m³)');
+    
+    const requestPromise = page.waitForRequest('https://api.openaq.org/v2/locations/tiles/2/0/2.pbf?parameter=33');
+    await page.getByRole('combobox').first().selectOption('33');
+    expect (await requestPromise).toBeTruthy();
+    await expect(page.locator('body > div.map-legend > div > div.map-legend-section > div.map-legend-title > span.type-subtitle-3.text-smoke-120'))
+    .toHaveText('UFP count (particles/cm³)');
+  });
+
+  test('Select NO (ppm), assert network call and legend text', async ({ page }) => {
+
+    await expect(page.locator('body > div.map-legend > div > div.map-legend-section > div.map-legend-title > span.type-subtitle-3.text-smoke-120'))
+    .toHaveText('PM 2.5 (µg/m³)');
+    
+    const requestPromise = page.waitForRequest('https://api.openaq.org/v2/locations/tiles/2/0/2.pbf?parameter=35');
+    await page.getByRole('combobox').first().selectOption('35');
+    expect (await requestPromise).toBeTruthy();
+    await expect(page.locator('body > div.map-legend > div > div.map-legend-section > div.map-legend-title > span.type-subtitle-3.text-smoke-120'))
+    .toHaveText('NO (ppm)');
+  });
+
+  test('Select PM1 count (particles/cm³), assert network call and legend text', async ({ page }) => {
+
+    await expect(page.locator('body > div.map-legend > div > div.map-legend-section > div.map-legend-title > span.type-subtitle-3.text-smoke-120'))
+    .toHaveText('PM 2.5 (µg/m³)');
+    
+    const requestPromise = page.waitForRequest('https://api.openaq.org/v2/locations/tiles/2/3/1.pbf?parameter=126');
+    await page.getByRole('combobox').first().selectOption('126');
+    expect (await requestPromise).toBeTruthy();
+    await expect(page.locator('body > div.map-legend > div > div.map-legend-section > div.map-legend-title > span.type-subtitle-3.text-smoke-120'))
+    .toHaveText('PM1 count (particles/cm³)');
+  });
+
+  test('Select PM2.5 count (particles/cm³), assert network call and legend text', async ({ page }) => {
+
+    await expect(page.locator('body > div.map-legend > div > div.map-legend-section > div.map-legend-title > span.type-subtitle-3.text-smoke-120'))
+    .toHaveText('PM 2.5 (µg/m³)');
+    
+    const requestPromise = page.waitForRequest('https://api.openaq.org/v2/locations/tiles/2/3/1.pbf?parameter=130');
+    await page.getByRole('combobox').first().selectOption('130');
+    expect (await requestPromise).toBeTruthy();
+    await expect(page.locator('body > div.map-legend > div > div.map-legend-section > div.map-legend-title > span.type-subtitle-3.text-smoke-120'))
+    .toHaveText('PM2.5 count (particles/cm³)');
+  });
+
+  test('Select PM10 count (particles/cm³), assert network call and legend text', async ({ page }) => {
+
+    await expect(page.locator('body > div.map-legend > div > div.map-legend-section > div.map-legend-title > span.type-subtitle-3.text-smoke-120'))
+    .toHaveText('PM 2.5 (µg/m³)');
+    
+    const requestPromise = page.waitForRequest('https://api.openaq.org/v2/locations/tiles/2/3/1.pbf?parameter=135');
+    await page.getByRole('combobox').first().selectOption('135');
+    expect (await requestPromise).toBeTruthy();
+    await expect(page.locator('body > div.map-legend > div > div.map-legend-section > div.map-legend-title > span.type-subtitle-3.text-smoke-120'))
+    .toHaveText('PM10 count (particles/cm³)');
+  });
+
+  test('Select NOx (ppm), assert network call and legend text', async ({ page }) => {
+
+    await expect(page.locator('body > div.map-legend > div > div.map-legend-section > div.map-legend-title > span.type-subtitle-3.text-smoke-120'))
+    .toHaveText('PM 2.5 (µg/m³)');
+    
+    const requestPromise = page.waitForRequest('https://api.openaq.org/v2/locations/tiles/2/3/1.pbf?parameter=19840');
+    await page.getByRole('combobox').first().selectOption('19840');
+    expect (await requestPromise).toBeTruthy();
+    await expect(page.locator('body > div.map-legend > div > div.map-legend-section > div.map-legend-title > span.type-subtitle-3.text-smoke-120'))
+    .toHaveText('NOx (ppm)');
+  });
+
+  test('Select NO mass (µg/m³), assert network call and legend text', async ({ page }) => {
+
+    await expect(page.locator('body > div.map-legend > div > div.map-legend-section > div.map-legend-title > span.type-subtitle-3.text-smoke-120'))
+    .toHaveText('PM 2.5 (µg/m³)');
+    
+    const requestPromise = page.waitForRequest('https://api.openaq.org/v2/locations/tiles/2/3/1.pbf?parameter=19843');
+    await page.getByRole('combobox').first().selectOption('19843');
+    expect (await requestPromise).toBeTruthy();
+    await expect(page.locator('body > div.map-legend > div > div.map-legend-section > div.map-legend-title > span.type-subtitle-3.text-smoke-120'))
+    .toHaveText('NO mass (µg/m³)');
+  });
+
+  test('Select PM4 (µg/m³), assert network call and legend text', async ({ page }) => {
+
+    await expect(page.locator('body > div.map-legend > div > div.map-legend-section > div.map-legend-title > span.type-subtitle-3.text-smoke-120'))
+    .toHaveText('PM 2.5 (µg/m³)');
+    
+    const requestPromise = page.waitForRequest('https://api.openaq.org/v2/locations/tiles/2/3/1.pbf?parameter=19844');
+    await page.getByRole('combobox').first().selectOption('19844');
+    expect (await requestPromise).toBeTruthy();
+    await expect(page.locator('body > div.map-legend > div > div.map-legend-section > div.map-legend-title > span.type-subtitle-3.text-smoke-120'))
+    .toHaveText('PM4 (µg/m³)');
+  });
   
+  // end of test suite
 });
--- tests/openaq.spec.js
+++ tests/openaq.spec.js
@@ -22,4 +22,14 @@ test.describe('openaq page', () => {
         expect(response.status()).toBe(200);
       });
 
+    test('lnkR test', async ({ page }) => { 
+        const requestPromise = page.waitForRequest('https://openaq-staging.org/why-air-quality/');
+        await page.locator('body > header > div > nav > ul > li:nth-child(2) > a').click(),
+        expect (await requestPromise).toBeTruthy();
+    });
+
+    // // Alternative way with a predicate. Note no await.
+    // const requestPromise = page.waitForRequest(request => request.url() === 'https://example.com' && request.method() === 'GET');
+    // await page.getByText('trigger request').click();
+    // const request = await requestPromise;
 });
