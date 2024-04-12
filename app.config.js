37582e5b3eebf41b3f587d7818117888ee643db6
Russ Biggs <russbiggs@gmail.com>
2024-04-11 20:18:53 -0600

    initial cdk

--- app.config.js
+++ app.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from '@solidjs/start/config';
 
 export default defineConfig({
+  server: {
+    preset: 'aws-lambda',
+  },
   start: {
     middleware: './src/middleware.ts',
   },
