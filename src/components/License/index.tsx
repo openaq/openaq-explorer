7d6d45a49351eca23536641967b87ef603bff994
Helena Skagerlid <helenaskagerlid@gmail.com>
2025-02-17 16:21:07 +0100

    style license logos + add hover text
    Co-authored-by: Rebecka Göthlin <RebeckaGothlin@users.noreply.github.com>

--- src/components/License/index.tsx
+++ src/components/License/index.tsx
@@ -1,5 +1,6 @@
 import { Show } from 'solid-js';
 import { LicenseDefinition } from './types';
+import '~/assets/scss/components/license.scss';
 
 import CCIcon from '../../assets/imgs/license-icons/cc-icon.svg';
 import NCIcon from '../../assets/imgs/license-icons/nc-icon.svg';
@@ -11,30 +12,47 @@ export const License = (props: LicenseDefinition) => {
   return (
     <div>
       <div>
-        <p class="license-name">{props.name}</p>
+        {/* <p class="license-name">{props.name}</p> */}
         <a rel="noopener noreferrer" target="_blank" href={props?.sourceUrl}>
-          {props?.sourceUrl}
+          {/* {props?.sourceUrl} */}
+          {props.name}
         </a>
       </div>
-      <div>
+      <div class="license-attribute-icons">
         <Show when={[30, 32, 41].includes(props?.id)}>
-          <CCIcon width={32} height={32} fill={'#5a6672'} />
+          <div class="tooltip" title="Creative Commons">
+            <CCIcon
+              role="img"
+              aria-label="Creative Commons Logo"
+              width={32}
+              height={32}
+              fill={'#5a6672'}
+            />
+          </div>
         </Show>
 
         <Show when={!props.commercialUseAllowed}>
-          <NCIcon width={32} height={32} fill={'#5a6672'} />
+          <div class="tooltip" title="Commercial Use Allowed">
+            <NCIcon width={32} height={32} fill={'#5a6672'} />
+          </div>
         </Show>
 
         <Show when={props.attributionRequired}>
-          <BYIcon width={32} height={32} fill={'#5a6672'} />
+          <div class="tooltip" title="Attribution Required">
+            <BYIcon width={32} height={32} fill={'#5a6672'} />
+          </div>
         </Show>
 
         <Show when={props.shareAlikeRequired}>
-          <SAIcon width={32} height={32} fill={'#5a6672'} />
+          <div class="tooltip" title="Share Alike Required">
+            <SAIcon width={32} height={32} fill={'#5a6672'} />
+          </div>
         </Show>
 
         <Show when={!props.modificationAllowed}>
-          <NDIcon width={32} height={32} fill={'#5a6672'} />
+          <div class="tooltip" title="Modification Allowed">
+            <NDIcon width={32} height={32} fill={'#5a6672'} />
+          </div>
         </Show>
       </div>
     </div>
