085d476769288d9b82700f2b23cc61a2a3be7f24
RebeckaGothlin <rebecka.gothlin@hotmail.com>
2025-02-19 10:51:36 +0100

    add env variable + render notification card content
    Co-authored-by: Helena Skagerlid <helenaskagerlid@users.noreply.github.com>

--- src/components/Cards/NotificationCard.tsx
+++ src/components/Cards/NotificationCard.tsx
@@ -1,22 +1,63 @@
+import { createSignal } from 'solid-js';
+import { ErrorIcon } from '~/assets/imgs/svgs/notification-icons/error-svg';
+import { InfoIcon } from '~/assets/imgs/svgs/notification-icons/info-svg';
 import { WarningIcon } from '~/assets/imgs/svgs/notification-icons/warning-svg';
 import '~/assets/scss/components/notification-card.scss';
 
 export const NotificationCard = () => {
+  const [showCard, setShowCard] = createSignal(true);
+
+  const notificationType = import.meta.env.VITE_NOTIFICATION_TYPE;
+  const notificationTitle = import.meta.env.VITE_NOTIFICATION_TITLE;
+  const notificationText = import.meta.env.VITE_NOTIFICATION_TEXT;
+
+  const getNotificationIcon = () => {
+    switch (notificationType) {
+      case 'warning':
+        return (
+          <>
+            <WarningIcon
+              role="img"
+              aria-label="Warning Icon"
+              class="notification-icon"
+            />
+          </>
+        );
+      case 'error':
+        return (
+          <ErrorIcon
+            role="img"
+            aria-label="Error Icon"
+            class="notification-icon"
+          />
+        );
+      case 'info':
+      default:
+        return (
+          <InfoIcon
+            role="img"
+            aria-label="Info Icon"
+            class="notification-icon"
+          />
+        );
+    }
+  };
+
   return (
     <>
-      <section class="notification-card">
-        <div class="notification-card__header">
-          <WarningIcon role="img" aria-label="Warning Icon" width={32} height={32} class="notification-icon"  />
-          {/* <ErrorIcon role="img" aria-label="Warning Icon" width={32} height={32}  />
-          <InfoIcon role="img" aria-label="Warning Icon" width={32} height={32}  /> */}
-          <h3>Title</h3>
-        </div>
-        <p>
-        If this notification goes away, assume everything is fine. Or horribly broken.
-        </p>
+      {showCard() && (
+        <section class="notification-card">
+          <div class="notification-card__header">
+            {getNotificationIcon()}
+            <h3>{notificationTitle}</h3>
+          </div>
+          <p>{notificationText}</p>
 
-        <button class="notification-btn">Dismiss</button>
-      </section>
+          <button class="notification-btn" onClick={() => setShowCard(false)}>
+            Dismiss
+          </button>
+        </section>
+      )}
     </>
   );
 };
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,18 +1,19 @@
-import path from 'path'
-import solid from "vite-plugin-solid"
-import { defineConfig } from "vitest/config"
+import path from 'path';
+import solid from 'vite-plugin-solid';
+import { defineConfig } from 'vitest/config';
+import solidSvg from 'vite-plugin-solid-svg';
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [solid(), solidSvg()],
 
   test: {
     globals: true,
     environment: 'jsdom',
   },
   resolve: {
-    conditions: ["development", "browser"],
+    conditions: ['development', 'browser'],
     alias: {
-      '~': path.resolve(__dirname, './src')
+      '~': path.resolve(__dirname, './src'),
     },
   },
 });
