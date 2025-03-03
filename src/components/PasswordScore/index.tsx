d801d7f41b536fa683bd0719e4a84256ca960781
Helena Skagerlid <helenaskagerlid@gmail.com>
2025-03-03 16:26:39 +0100

    WIP insert SVG to passwordscore

--- src/components/PasswordScore/index.tsx
+++ src/components/PasswordScore/index.tsx
@@ -9,38 +9,41 @@ import {
 
 import '~/assets/scss/components/password-score.scss';
 
-const errorSvg = 'warning_fire100.svg' as SvgSymbol;
-const checkSvg = 'check_mantis100.svg' as SvgSymbol;
+import ErrorIcon from '~/assets/imgs/warning.svg';
+import CheckIcon from '~/assets/imgs/check.svg';
+
+// const errorSvg = 'warning_fire100.svg' as SvgSymbol;
+// const checkSvg = 'check_mantis100.svg' as SvgSymbol;
 
 function passwordValues(score: number): PasswordValue {
   let color: Color;
   let message: string;
-  let symbol: SvgSymbol;
+  let symbol: Component<SvgSVGAttributes<SVGSVGElement>>;
   switch (score) {
     case 0:
       color = 'warning';
-      symbol = errorSvg;
+      symbol = ErrorIcon;
       message = `Very weak password`;
       break;
     case 1:
       color = 'warning';
-      symbol = errorSvg;
+      symbol = ErrorIcon;
       message = `Weak password`;
       break;
     case 2:
       color = 'alert';
-      symbol = errorSvg;
+      symbol = ErrorIcon;
       message = `Somewhat secure password`;
       break;
     case 3:
       color = 'ok';
       message = 'Strong password';
-      symbol = checkSvg;
+      symbol = CheckIcon;
       break;
     case 4:
       color = 'ok';
       message = 'Very strong password';
-      symbol = checkSvg;
+      symbol = CheckIcon;
       break;
     default:
       color = 'warning';
