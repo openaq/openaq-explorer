8c8aaa14b2d5bdf9bfbf732194b9e60d6c5880d5
Russ Biggs <russbiggs@gmail.com>
2025-03-03 16:39:20 -0700

    svg type fix password score and refactor

--- src/components/PasswordScore/index.tsx
+++ src/components/PasswordScore/index.tsx
@@ -1,9 +1,8 @@
-import { Show } from 'solid-js';
+import { Match, Show, Switch } from 'solid-js';
 import {
   StrengthBarDefinition,
   PasswordScoreDefinition,
   Color,
-  SvgSymbol,
   PasswordValue,
 } from './types';
 
@@ -12,48 +11,37 @@ import '~/assets/scss/components/password-score.scss';
 import ErrorIcon from '~/assets/imgs/warning.svg';
 import CheckIcon from '~/assets/imgs/check.svg';
 
-// const errorSvg = 'warning_fire100.svg' as SvgSymbol;
-// const checkSvg = 'check_mantis100.svg' as SvgSymbol;
-
 function passwordValues(score: number): PasswordValue {
   let color: Color;
   let message: string;
-  let symbol: Component<SvgSVGAttributes<SVGSVGElement>>;
   switch (score) {
     case 0:
       color = 'warning';
-      symbol = ErrorIcon;
       message = `Very weak password`;
       break;
     case 1:
       color = 'warning';
-      symbol = ErrorIcon;
       message = `Weak password`;
       break;
     case 2:
       color = 'alert';
-      symbol = ErrorIcon;
       message = `Somewhat secure password`;
       break;
     case 3:
       color = 'ok';
       message = 'Strong password';
-      symbol = CheckIcon;
       break;
     case 4:
       color = 'ok';
       message = 'Very strong password';
-      symbol = CheckIcon;
       break;
     default:
       color = 'warning';
-      symbol;
       message = '';
   }
   return {
     color: color,
     message: message,
-    symbol: symbol,
   };
 }
 
@@ -70,6 +58,8 @@ export function StrengthBar(props: StrengthBarDefinition) {
 }
 
 export default function PasswordScore(props: PasswordScoreDefinition) {
+
+
   return (
     <div class="password-strength">
       <div class="strength-meter-container">
@@ -110,11 +100,14 @@ export default function PasswordScore(props: PasswordScoreDefinition) {
         </Show>
         <span class="strength-message">
           {passwordValues(props.score!).message}
-          {passwordValues(props.score!).symbol ? (
-            <img src={`/svgs/${passwordValues(props.score!).symbol}`} alt="" />
-          ) : (
-            ''
-          )}
+          <Switch fallback={''}>
+            <Match when={props.score! < 3}>
+              <ErrorIcon fill="#dd443c"/>
+            </Match>
+            <Match when={props.score! >= 3}>
+              <CheckIcon fill="#89c053" />
+            </Match>
+          </Switch>
         </span>
         <span class="type-body-1">{props.warning}</span>
       </div>
