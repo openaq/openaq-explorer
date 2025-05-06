9ae6bc385616b40529b9690dd7f5f89c10bfbd38
RebeckaGothlin <rebecka.gothlin@hotmail.com>
2025-05-06 13:03:38 +0200

    add link to getting started

--- src/routes/(root)/locations/[id].tsx
+++ src/routes/(root)/locations/[id].tsx
@@ -65,6 +65,11 @@ export default function Location() {
                   <DownloadCard {...location()} />
                 </Show>
               </section>
+              <section class="getting-started-section">
+                <a href="/getting-started" class="getting-started-link">
+                  Learn how to use the Explorer
+                </a>
+              </section>
             </Show>
           </main>
         </Show>
